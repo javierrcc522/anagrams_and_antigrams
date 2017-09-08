6cacb8803fd2980b523997070a17375c2bb4bdf8
Javier Jimenez <javierrcc522@gmail.com>
2017-09-08 10:00:57 -0700

    a custom class with instances

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,7 +1,16 @@
 #! user/bin/env ruby
 class AnAnti
-  def initialize
-
-    
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
   end
+
+  def compare_words()
+
+  end
+
+
+
+
+
 end

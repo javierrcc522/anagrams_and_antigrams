c9a4042870f16213a53760d70cc2fcaed27762b5
Javier Jimenez <javierrcc522@gmail.com>
2017-09-08 10:18:55 -0700

    a method that downcases any word and checks if it's equal

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,16 +1,10 @@
 #! user/bin/env ruby
-class AnAnti
-  def initialize(word1, word2)
-    @word1 = word1
-    @word2 = word2
+class Anagram
+  def compare_words(word1, word2)
+    if word1.downcase.match((/[AEIOU]+/i)) && word2.downcase.match((/[AEIOU]+/i))
+    return true
+    else
+    return false
+    end
   end
-
-  def compare_words()
-
-  end
-
-
-
-
-
 end

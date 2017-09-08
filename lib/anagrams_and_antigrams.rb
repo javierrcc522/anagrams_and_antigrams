d5ef8b2bcdc2379782ed42872032f53b1e5223b4
Javier Jimenez <javierrcc522@gmail.com>
2017-09-08 11:41:18 -0700

    the initialize the custom class

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,7 +1,7 @@
 #! user/bin/env ruby
 class Anagram
   def compare_words(word1, word2)
-    if word1.downcase.match((/[AEIOU]+/i)) && word2.downcase.match((/[AEIOU]+/i))
+    if word1.length !=0 && word1.match((/[AEIOU]+/i)) && word2.length !=0 && word2.match((/[AEIOU]+/i))
     return true
     else
     return false
